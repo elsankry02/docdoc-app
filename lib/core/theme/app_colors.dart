d4849e5f7d8a135a7f94868f2c7976deb10d181c
elsankry02 <elsankry02@gmail.com>
2026-03-22 09:39:20 +0200

    feat: add App colors

---
+++ lib/core/theme/app_colors.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+
+abstract class AppColors {
+  // Background
+  static const white = Color(0xffFFFFFF);
+  static const body = Color(0xff7A7A7A);
+  static const black = Color(0xff161616);
+  // Grey Color
+  static const grey100 = Color(0xff242424);
+  static const grey90 = Color(0xff404040);
+  static const grey80 = Color(0xff616161);
+  static const grey70 = Color(0xff757575);
+  static const grey60 = Color(0xff9E9E9E);
+  static const grey50 = Color(0xffC2C2C2);
+  static const grey40 = Color(0xffE0E0E0);
+  static const grey30 = Color(0xffEDEDED);
+  static const grey20 = Color(0xffF5F5F5);
+  // Primary Color
+  static const primary100 = Color(0xff247cff);
+  static const primary80 = Color(0xff5096ff);
+  static const primary60 = Color(0xff7cb0ff);
+  static const primary40 = Color(0xffd3e5ff);
+  static const primary20 = Color(0xffeaf2ff);
+  static const primarySurface = Color(0xffa7cbff);
+  // Secondary
+  static const surfaceBlue = Color(0xffeaf2ff);
+  static const surfaceGreen = Color(0xffe9faef);
+  static const surfaceRed = Color(0xffeaf2ff);
+  static const fillblue = Color(0xff247cff);
+  static const fillGreen = Color(0xff22c55e);
+  static const fillRed = Color(0xffff4c5e);
+  static const surfaceText = Color(0xfffafafb);
+  static const form = Color(0xfffdfdff);
+  static const chat = Color(0xfff8f9fd);
+  // Warning
+  static const warning100 = Color(0xffffd600);
+  static const warning80 = Color(0xffffdd2a);
+  static const warning60 = Color(0xffffe455);
+  static const warning40 = Color(0xffffea80);
+  static const warning20 = Color(0xfffff1aa);
+  static const warningSurface = Color(0xfffff7cc);
+}
