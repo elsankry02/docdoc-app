4a1e4a7e91619fd8aedd4510ca348053ef497de5
elsankry02 <elsankry02@gmail.com>
2026-03-22 09:39:33 +0200

    feat: implement DocDocApp with theme support

---
+++ lib/app/app.dart
@@ -0,0 +1,16 @@
+import 'package:docdoc_app/core/theme/app_theme_data.dart';
+import 'package:flutter/material.dart';
+
+class DocDocApp extends StatelessWidget {
+  const DocDocApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      darkTheme: AppThemeData.darkTheme,
+      theme: AppThemeData.lightTheme,
+      themeMode: .light,
+    );
+  }
+}
