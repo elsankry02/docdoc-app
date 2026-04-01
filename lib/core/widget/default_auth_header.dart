c4cc88175c9c51683853a3283f081edc311e118a
elsankry02 <elsankry02@gmail.com>
2026-04-01 16:20:23 +0200

    feat: add DefaultAuthHeader widget for authentication header display

---
+++ lib/core/widget/default_auth_header.dart
@@ -0,0 +1,40 @@
+import 'package:docdoc_app/core/theme/app_colors.dart';
+import 'package:docdoc_app/helpers/extensions/extensions.dart';
+import 'package:flutter/material.dart';
+
+class DefaultAuthHeader extends StatelessWidget {
+  final String title;
+  final String subTitle;
+  const DefaultAuthHeader({
+    super.key,
+    required this.title,
+    required this.subTitle,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      spacing: 8,
+      children: [
+        Text(
+          title,
+          maxLines: 1,
+          overflow: .ellipsis,
+          style: context.textTheme.headlineSmall?.copyWith(
+            fontWeight: FontWeight.w700,
+            color: AppColors.primary100,
+          ),
+        ),
+        Text(
+          subTitle,
+          maxLines: 3,
+          overflow: .ellipsis,
+          style: context.textTheme.labelLarge?.copyWith(
+            fontWeight: FontWeight.w400,
+            color: AppColors.body,
+          ),
+        ),
+      ],
+    );
+  }
+}
