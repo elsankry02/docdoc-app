cce1c5a1605848175b0b985b2a076475a9584440
elsankry02 <elsankry02@gmail.com>
2026-04-13 10:02:14 +0200

    fix: update scaffoldBackgroundColor for light and dark themes

--- lib/core/theme/app_theme_data.dart
+++ lib/core/theme/app_theme_data.dart
@@ -1,13 +1,16 @@
+import 'package:docdoc_app/core/theme/app_colors.dart';
 import 'package:flutter/material.dart';
 
 import '../../helpers/constants/app_strings.dart';
 
 abstract class AppThemeData {
   static final lightTheme = ThemeData(
+    scaffoldBackgroundColor: AppColors.white,
     fontFamily: AppStrings.inter,
     brightness: .light,
   );
   static final darkTheme = ThemeData(
+    scaffoldBackgroundColor: AppColors.black,
     fontFamily: AppStrings.inter,
     brightness: .dark,
   );
