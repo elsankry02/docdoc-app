37ae31e78278d858c1d93c772f444ebdbbefc134
elsankry02 <elsankry02@gmail.com>
2026-04-14 07:28:32 +0200

    feat: update splash screen assets and remove unused SplashPage component

--- lib/core/routing/router.dart
+++ lib/core/routing/router.dart
@@ -4,7 +4,6 @@ import '../../features/auth/ui/forget_pasword/view/forget_password.dart';
 import '../../features/auth/ui/sign_in/view/signin_page.dart';
 import '../../features/auth/ui/sign_up/view/signup_page.dart';
 import '../../features/on_boarding/view/on_boarding_page.dart';
-import '../../features/splash/view/splash_page.dart';
 import 'routes.dart';
 
 part 'router.gr.dart';
@@ -15,7 +14,6 @@ final router = AppRouter();
 class AppRouter extends RootStackRouter {
   @override
   List<AutoRoute> get routes => [
-    // AutoRoute(page: SplashRoute.page, path: Routes.splash),
     AutoRoute(
       page: OnBoardingRoute.page,
       path: Routes.onBoarding,
