01d5ff136804704c4fe56d568b3a5930ac87e86d
elsankry02 <elsankry02@gmail.com>
2026-04-16 11:46:54 +0200

    fix: resolve merge conflicts in router.dart and clean up imports

--- lib/core/routing/router.dart
+++ lib/core/routing/router.dart
@@ -1,17 +1,9 @@
 import 'package:auto_route/auto_route.dart';
-<<<<<<< HEAD
 
-=======
->>>>>>> features/login
 import '../../features/auth/ui/forget_pasword/view/forget_password.dart';
 import '../../features/auth/ui/sign_in/view/signin_page.dart';
 import '../../features/auth/ui/sign_up/view/signup_page.dart';
 import '../../features/on_boarding/view/on_boarding_page.dart';
-<<<<<<< HEAD
-=======
-import '../../features/splash/view/splash_page.dart';
-
->>>>>>> features/login
 import 'routes.dart';
 
 part 'router.gr.dart';
